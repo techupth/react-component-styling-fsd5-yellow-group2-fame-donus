cb871b940246f07a58138bdbf35236c05b871c76
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-12-20 16:17:17 +0700

    feat:Ex.component styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,30 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Button() {}
+function Button(props){
+    if (props.type === "primary") {
+      return (
+        <button  
+          css={css`
+          background-color: #074EE8;
+          height: 50px;
+          width: 170px;
+          border-radius: 4px;
+          margin: 4px; 
+        `}>Large</button>
+      )
+    } else if (props.type === "secondary") {
+      return (
+        <button  
+          css={css`
+          background-color: #07A4E8;
+          height: 50px;
+          width: 170px;
+          border-radius: 4px;
+          margin: 4px; 
+        `}>Large</button>
+      )
+    }
+  }
+  export default Button
